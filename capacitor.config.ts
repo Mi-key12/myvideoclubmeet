befedbce718fdb4e31e4da02abe47b5143e45193
Mi-key12 <100189415+Mi-key12@users.noreply.github.com>
2022-02-28 19:09:53 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myvideoclubmeet',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
