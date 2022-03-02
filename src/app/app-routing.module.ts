a875f664ceba00c7474d9563c76993df7562ee92
Mi-key12 <100189415+Mi-key12@users.noreply.github.com>
2022-03-02 03:35:22 -0700

    Error solved #2
    Correct syntax for routerLink, do not use ion-router-link if no vanilla or stencil are implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+import { AutorPage } from './autor/autor.page';
+import { CurriculumPage } from './curriculum/curriculum.page';
+import { VideoclubPage } from './videoclub/videoclub.page';
 
 const routes: Routes = [
   {
     path: 'home',
+    component: HomePage,
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'autor',
+    component: AutorPage,
     loadChildren: () => import('./autor/autor.module').then( m => m.AutorPageModule)
   },
   {
     path: 'curriculum',
+    component: CurriculumPage,
     loadChildren: () => import('./curriculum/curriculum.module').then( m => m.CurriculumPageModule)
   },
   {
     path: 'videoclub',
+    component: VideoclubPage,
     loadChildren: () => import('./videoclub/videoclub.module').then( m => m.VideoclubPageModule)
   },
   {
